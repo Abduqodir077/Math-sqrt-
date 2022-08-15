eff35b0e9ca31b5fe3521a7758f3c524a2953193
Abduqodir077 <urinovabduqodir07@gmail.com>
2022-08-15 19:22:24 +0500

    project start

---
+++ js.js
@@ -0,0 +1,10 @@
+let par = document.getElementById("demo");
+// let outputScreen = document.getElementById("output-screen");
+
+// function display(num){
+//     outputScreen.value += num;
+// }
+
+par.innerHTML = Math.sqrt(64);
+
+
